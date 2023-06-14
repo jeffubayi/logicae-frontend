99b75ea26a5cb45ddf0d067d45a795cbf4aee93b
jeffubayi <jeffubayi@gmail.com>
2023-06-14 20:19:00 +0300

    add types definitions

--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,27 @@
 export type JokesState = {
-    id: number;
-    Title: string;
-    Author: string;
-    Body?:string;
-    Views: number;
-    CreatedAt: Date;
+  id: number;
+  Title: string;
+  Author: string;
+  Body: string;
+  Views: number;
+  CreatedAt: Date | number;
+};
+
+export type JokesValues = {
+  id?: number | string | undefined;
+  Title: string | string[] | undefined;
+  Author: string | string[] | undefined;
+  Body?: string | string[] | undefined;
+  Views: string | string[] | undefined;
+  CreatedAt?: Date | string | string[];
 };
 
 export interface UserProfileState {
-    userId?:string;
-    userEmail?: string;
-    accessToken?: string;
+  userId?: string;
+  userEmail?: string;
+  accessToken?: string;
 }
 
-export  interface RootState {
-    userProfile:UserProfileState; 
+export interface RootState {
+  userProfile: UserProfileState;
 }
-
-
