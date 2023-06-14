cc0b62f3205e952cc052a738683a3ecc8ca4eed1
jeffubayi <jeffubayi@gmail.com>
2023-06-14 20:34:24 +0300

    add pagination

--- components/table.tsx
+++ components/table.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { DataGrid, GridToolbar, GridRowParams, GridColDef } from '@mui/x-data-grid';
 import { Box } from '@mui/material';
-import { useRouter } from "next/router";
+import { useLocalStorage } from 'react-use';
 
 import { JokesState } from "../types";
 import CardTitle from "./list";
@@ -10,43 +10,35 @@ interface Props {
     rows: JokesState[];
     columns: GridColDef<JokesState>[];
     loading: boolean;
+    onRowClick:(params: GridRowParams) => void;
 }
 
 export default function Table(props: Props) {
-    const { rows, columns, loading } = props
-    const router = useRouter();
-
-    const handleTitleClick = (
-        params: GridRowParams,
-    ) => {
-        router.push(
-            {
-                pathname: "/jokes/[id]",
-                query: {
-                    id: params.id,
-                    method: "Edit",
-                },
-            },
-            `/jokes/edit/${params.id}`
-        );
-    };
+    const { rows, columns, loading,onRowClick } = props
+    const [paginationModel, setPaginationModel] = useLocalStorage("pagination",{
+        pageSize: 5,
+        page: 0,
+    });
 
     return (
         <Box sx={{ p: 2 }}>
             <CardTitle />
             <DataGrid
-                sx={{ borderRadius: "1rem", bgcolor: 'background.paper' }}
                 autoHeight
                 pagination
-                showColumnVerticalBorder
+                showCellVerticalBorder
                 disableColumnSelector
                 disableDensitySelector
+                disableColumnMenu
+                sortingOrder={['asc', 'desc']}
                 rows={rows}
                 columns={columns}
                 loading={loading}
                 getRowId={(row) => row.id}
-                onRowClick={handleTitleClick}
+                onRowClick={onRowClick}
                 pageSizeOptions={[5, 10]}
+                paginationModel={paginationModel}
+                onPaginationModelChange={setPaginationModel}
                 slots={{ toolbar: GridToolbar }}
                 slotProps={{
                     toolbar: {
@@ -57,10 +49,11 @@ export default function Table(props: Props) {
                     },
                 }}
                 initialState={{
-                    pagination: {
-                        paginationModel: { pageSize: 5, page: 0 },
+                    sorting: {
+                        sortModel: [{ field: 'Views', sort: 'desc' }, { field: 'CreatedAt', sort: 'desc' }],
                     },
                 }}
+                sx={{ borderRadius: "1rem", bgcolor: 'background.paper' }}
             />
         </Box>
     );
