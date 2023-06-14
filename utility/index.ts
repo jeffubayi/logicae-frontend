db330ac1ea8b02557d44d9aac68502ff6e7751fc
jeffubayi <jeffubayi@gmail.com>
2023-06-14 23:37:45 +0300

    add date formater

--- utility/index.ts
+++ utility/index.ts
@@ -1,22 +1,14 @@
-export function timeConverter(UNIX_timestamp: number) {
-  var a = new Date(UNIX_timestamp);
-  var months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  var year = a.getFullYear();
-  var month = months[a.getMonth()];
-  var date = a.getDate();
-  var time = date + " " + month + " " + year;
-  return time;
-}
+export const timeConverter = (timestamp: number) => {
+  if (!timestamp) {
+    return timestamp;
+  }
+
+  let date = new Date(timestamp);
+  let formatedDate = date.toLocaleDateString("en-GB", {
+    day: "numeric",
+    year: "numeric",
+    month: "long",
+  }); // DD MM YYYY
+
+  return formatedDate;
+};
